38f02e5a41db72bf4ab1e1af0d28fe5cce1b6745
Rukey7 <deadlong7@gmail.com>
2016-08-25 22:29:31 +0800

    添加专题界面

---
+++ app/src/main/java/com/dl7/myapp/module/special/SpecialActivity.java
@@ -0,0 +1,23 @@
+package com.dl7.myapp.module.special;
+
+import com.dl7.myapp.R;
+import com.dl7.myapp.module.base.BaseActivity;
+
+public class SpecialActivity extends BaseActivity {
+
+    @Override
+    protected int attachLayoutRes() {
+        return R.layout.activity_special;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void updateViews() {
+
+    }
+
+}
