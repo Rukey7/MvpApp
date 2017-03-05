ae4ebc614fee5c4349b0771393a8a4658cbc4397
Rukey7 <deadlong7@gmail.com>
2017-03-05 19:33:38 +0800

    修改专题界面浮动按钮消失后出不来

--- app/src/main/java/com/dl7/mvp/widget/behavior/ScrollAwareFABBehavior.java
+++ app/src/main/java/com/dl7/mvp/widget/behavior/ScrollAwareFABBehavior.java
@@ -26,6 +26,7 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
                                        FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
                 super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
                         nestedScrollAxes);
@@ -36,7 +37,6 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
                                View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
                 dyUnconsumed);
-
         if (dyConsumed > 0 && !mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
 //            child.hide();
             animateOut(child);
